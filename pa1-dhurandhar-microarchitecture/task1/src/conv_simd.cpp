706e3e69ff020df2ea2e14785c2096fbbdc440d9
grey_cat <eduapoorvlearning@gmail.com>
2026-09-03 15:22:41 +0530

    added simd128_v3 which now uses 2 128 bit registers simultaneously so 8x resusing same ker val even with 128 bit reg

--- pa1-dhurandhar-microarchitecture/task1/src/conv_simd.cpp
+++ pa1-dhurandhar-microarchitecture/task1/src/conv_simd.cpp
@@ -48,6 +48,32 @@ void conv_simd128_v2(const float* in, float* out, const float* ker,
         }
     }
 }
+void conv_simd128_v3(const float* in, float* out, const float* ker,
+               int H, int W, int K) {
+    // TODO(student): replace this placeholder with your AVX2 implementation.
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;  // padded row stride
+
+    for (int oy = 0; oy < H; ++oy) {
+        for (int ox = 0; ox < W; ox+=8) {
+            __m128 acc1 = _mm_setzero_ps();
+            __m128 acc2 = _mm_setzero_ps();
+            for (int ky = 0; ky < K; ++ky) {
+                int in_offset = (oy + ky) * in_stride + ox;
+                int ker_offset = ky * K;
+                for (int kx = 0; kx < K; ++kx, in_offset++, ker_offset++) {
+                    __m128 in_val1 = _mm_loadu_ps(&in[in_offset]);
+                    __m128 in_val2 = _mm_loadu_ps(&in[in_offset + 4]);
+                    __m128 ker_val = _mm_set1_ps(ker[ker_offset]);
+                    acc1 = _mm_fmadd_ps(ker_val, in_val1, acc1);
+                    acc2 = _mm_fmadd_ps(ker_val, in_val2, acc2);
+                }
+            }
+            _mm_storeu_ps(&out[oy * W + ox], acc1);
+            _mm_storeu_ps(&out[oy * W + ox + 4], acc2);
+        }
+    }
+}
 void conv_simd256(const float* in, float* out, const float* ker,
                int H, int W, int K) {
     // TODO(student): replace this placeholder with your AVX2 implementation.
