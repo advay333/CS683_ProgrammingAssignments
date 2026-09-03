b228eea7ecba5c5206de3ddd244cb613a1138514
grey_cat <eduapoorvlearning@gmail.com>
2026-09-03 15:31:03 +0530

    fixed bug related to tails, added tail correction to all versions

--- pa1-dhurandhar-microarchitecture/task1/src/conv_simd.cpp
+++ pa1-dhurandhar-microarchitecture/task1/src/conv_simd.cpp
@@ -4,25 +4,34 @@
 #include "convolution.h"
 
 
+
 void conv_simd128_v1(const float* in, float* out, const float* ker,
                int H, int W, int K) {
     // TODO(student): replace this placeholder with your AVX2 implementation.
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
-
+    const int tail = W % 4;
     for (int oy = 0; oy < H; ++oy) {
         for (int ox = 0; ox < W; ox+=4) {
             __m128 acc = _mm_setzero_ps();
             for (int ky = 0; ky < K; ++ky) {
-                int in_offset = (oy + ky) * in_stride + ox;
-                int ker_offset = ky * K;
-                for (int kx = 0; kx < K; ++kx, in_offset++, ker_offset++) {
-                    __m128 in_val = _mm_loadu_ps(&in[in_offset]);
-                    __m128 ker_val = _mm_set1_ps(ker[ker_offset]);
+                for (int kx = 0; kx < K; ++kx) {
+                    __m128 in_val = _mm_loadu_ps(&in[(oy + ky) * in_stride + (ox + kx)]);
+                    __m128 ker_val = _mm_set1_ps(ker[ky * K + kx]);
                     acc = _mm_fmadd_ps(ker_val, in_val, acc);
                 }
             }
             _mm_storeu_ps(&out[oy * W + ox], acc);
+        }
+        for(int ox = W - tail; ox < W; ++ox){
+            float acc  = 0.0f;
+            for(int ky = 0; ky < K; ++ky) {
+                const int in_offset = (oy + ky) * in_stride + ox;
+                const int ker_offset = ky * K;
+                for (int kx = 0; kx < K; ++kx) {
+                    acc += ker[ker_offset + kx] * in[in_offset + kx];
+                }
+            }
         }
     }
 }
@@ -31,7 +40,7 @@ void conv_simd128_v2(const float* in, float* out, const float* ker,
     // TODO(student): replace this placeholder with your AVX2 implementation.
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
-
+    const int tail = W % 4;
     for (int oy = 0; oy < H; ++oy) {
         for (int ox = 0; ox < W; ox+=4) {
             __m128 acc = _mm_setzero_ps();
@@ -46,6 +55,16 @@ void conv_simd128_v2(const float* in, float* out, const float* ker,
             }
             _mm_storeu_ps(&out[oy * W + ox], acc);
         }
+        for(int ox = W - tail; ox < W; ++ox){
+            float acc  = 0.0f;
+            for(int ky = 0; ky < K; ++ky) {
+                const int in_offset = (oy + ky) * in_stride + ox;
+                const int ker_offset = ky * K;
+                for (int kx = 0; kx < K; ++kx) {
+                    acc += ker[ker_offset + kx] * in[in_offset + kx];
+                }
+            }
+        }
     }
 }
 void conv_simd128_v3(const float* in, float* out, const float* ker,
@@ -53,6 +72,7 @@ void conv_simd128_v3(const float* in, float* out, const float* ker,
     // TODO(student): replace this placeholder with your AVX2 implementation.
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
+    const int tail = W % 8;
 
     for (int oy = 0; oy < H; ++oy) {
         for (int ox = 0; ox < W; ox+=8) {
@@ -72,6 +92,16 @@ void conv_simd128_v3(const float* in, float* out, const float* ker,
             _mm_storeu_ps(&out[oy * W + ox], acc1);
             _mm_storeu_ps(&out[oy * W + ox + 4], acc2);
         }
+        for(int ox = W - tail; ox < W; ++ox){
+            float acc  = 0.0f;
+            for(int ky = 0; ky < K; ++ky) {
+                const int in_offset = (oy + ky) * in_stride + ox;
+                const int ker_offset = ky * K;
+                for (int kx = 0; kx < K; ++kx) {
+                    acc += ker[ker_offset + kx] * in[in_offset + kx];
+                }
+            }
+        }
     }
 }
 void conv_simd256_v1(const float* in, float* out, const float* ker,
@@ -79,7 +109,7 @@ void conv_simd256_v1(const float* in, float* out, const float* ker,
     // TODO(student): replace this placeholder with your AVX2 implementation.
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
-
+    const int tail = W % 8;
     for (int oy = 0; oy < H; ++oy) {
         for (int ox = 0; ox < W; ox+=8) {
             __m256 acc = _mm256_setzero_ps();
@@ -91,6 +121,17 @@ void conv_simd256_v1(const float* in, float* out, const float* ker,
                 }
             }
             _mm256_storeu_ps(&out[oy * W + ox], acc);
+        }
+        for(int ox = W - tail; ox < W; ++ox){
+            float acc  = 0.0f;
+            for(int ky = 0; ky < K; ++ky) {
+                const int in_offset = (oy + ky) * in_stride + ox;
+                const int ker_offset = ky * K;
+                for (int kx = 0; kx < K; ++kx) {
+                    acc += ker[ker_offset + kx] * in[in_offset + kx];
+                }
+            }
+            out[oy * W + ox] = acc;
         }
     }
 }
