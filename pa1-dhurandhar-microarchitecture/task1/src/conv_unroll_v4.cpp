7604f30244bcdd49aa64a90124213f37128b2e3a
rookie-apoorv <eduapoorvlearning@gmail.com>
2026-09-25 18:49:58 +0530

    sanity commit

--- pa1-dhurandhar-microarchitecture/task1/src/conv_unroll_v4.cpp
+++ pa1-dhurandhar-microarchitecture/task1/src/conv_unroll_v4.cpp
@@ -6,41 +6,33 @@
 
 void conv_unroll_v4(const float* in, float* out, const float* ker,
                  int H, int W, int K) {
-    // TODO(student): replace this placeholder with your unrolled implementation.
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
     const int W_tail = W % 2; 
     const int W_unrolled = W - W_tail;
     const int H_tail = H % 2;
     const int H_unrolled = H - H_tail;
-
     for (int oy = 0; oy < H_unrolled; oy+=2) {
-        
         for (int ox = 0; ox < W_unrolled; ox+=2) {
             float acc00 = 0.0f;
             float acc01 = 0.0f;
             float acc10 = 0.0f;
             float acc11 = 0.0f;
-
             for (int ky = 0; ky < K; ++ky) {
                 const int in_row = (oy + ky) * in_stride;
                 const int in_row_next = (oy + 1 + ky) * in_stride;
                 const int ker_row = ky * K;
-                
                 for (int kx = 0; kx < K-1; kx += 2) {
                     const float ker_val = ker[ker_row + kx];
                     const float ker_val_next = ker[ker_row + kx + 1];
                     acc00 += in[in_row + (ox + kx)] * ker_val;
                     acc00 += in[in_row + (ox + kx + 1)] * ker_val_next;
-
                     acc01 += in[in_row + (ox + 1 + kx)] * ker_val;
                     acc01 += in[in_row + (ox + 1 + kx + 1)] * ker_val_next;
-
                     acc10 += in[in_row_next + (ox + kx)] * ker_val;
                     acc10 += in[in_row_next + (ox + kx + 1)] * ker_val_next;
                     acc11 += in[in_row_next + (ox + 1 + kx)] * ker_val;
                     acc11 += in[in_row_next + (ox + 1 + kx + 1)] * ker_val_next;
-
                 }
                 const float ker_val_last = ker[ker_row + (K - 1)];
                 acc00 += in[in_row + (ox + K - 1)] * ker_val_last;
@@ -53,7 +45,6 @@ void conv_unroll_v4(const float* in, float* out, const float* ker,
             out[(oy + 1) * W + ox] = acc10;
             out[(oy + 1) * W + ox + 1] = acc11;
         }
-        // Handle the tail elements if W is odd
         if (W_tail == 1) {
             float acc0 = 0.0f;
             float acc1 = 0.0f;
@@ -61,7 +52,6 @@ void conv_unroll_v4(const float* in, float* out, const float* ker,
                 const int in_row = (oy + ky) * in_stride;
                 const int in_row_next = (oy + 1 + ky) * in_stride;
                 const int ker_row = ky * K;
-
                 for(int kx = 0; kx < K-1; kx += 2) {
                     const float ker_val = ker[ker_row + kx];
                     const float ker_val_next = ker[ker_row + kx + 1];
@@ -78,7 +68,6 @@ void conv_unroll_v4(const float* in, float* out, const float* ker,
             out[(oy + 1) * W + (W - 1)] = acc1;
         }
     }
-        // Handle the tail elements if H is odd
     if (H_tail == 1) {
         for(int ox = 0; ox < W_unrolled; ox+=2) {
             float acc0 = 0.0f;
